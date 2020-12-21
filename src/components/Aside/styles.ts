a47ba191f774ae7ff68b72f7efbe9aaa9a1d7cef
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-12-21 15:22:21 -0300

    Incluindo temas dark e light e seus arquivos necessários.

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     grid-area: AS;
-    background: #252A48;
+    background: ${props => props.theme.colors.secondary};
+    color: ${props => props.theme.colors.white};
 `;
