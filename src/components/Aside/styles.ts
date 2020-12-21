5fc8a5713ec63fe68b74ea221e1f340d93267ebb
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-12-21 17:04:35 -0300

    Incluindo menu lateral no sistema.

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,5 +3,47 @@ import styled from 'styled-components';
 export const Container = styled.div`
     grid-area: AS;
     background: ${props => props.theme.colors.secondary};
+    padding-left: 20px;
+    border-right: 1px solid ${props => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    height: 70px;
+`;
+
+export const LogoImg = styled.img`
+    height: 40px;
+    width: 40px;
+`;
+
+export const Title = styled.h3`
     color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+    margin-left: 20px
+`;
+
+export const MenuItemLink = styled.a`
+    display: flex;
+    align-items: center;
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+    transition: opacity .3s;
+    margin: 10px 0;
+
+    &:hover {
+        opacity: .7;
+    }
+
+    > svg {
+        font-size: 20px;
+        margin-right: 5px;
+    }
 `;
