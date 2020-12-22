d567df26301acab71f4feb0f5a3a9229a40443db
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-12-22 15:36:55 -0300

    Criando o content header do dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,75 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
+import gains from '../../repositories/gains';
+import expenses from '../../repositories/expenses';
+import listOfMonths from '../../utils/months';
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-    const options = [
-        {value: 'Alexsandro', label: 'Alexsandro Silva'},
-        {value: 'Alexsandro', label: 'Alexsandro Silva'},
-        {value: 'Alexsandro', label: 'Alexsandro Silva'},
-    ]
+    const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
+    const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
+
+    const years = useMemo(() => {
+        let uniqueYears: number[] = [];
+
+        [...expenses, ...gains].forEach(element => {
+            const date = new Date(element.date);
+            const year = date.getFullYear();
+
+            if (!uniqueYears.includes(year)) {
+                uniqueYears.push(year);
+            }
+        });
+
+        return uniqueYears.map(year => {
+            return {
+                value: year,
+                label: year,
+            }
+        });
+    }, []);
+
+    const months = useMemo(() => {
+        return listOfMonths.map((month, index) => {
+            return {
+                value: index + 1,
+                label: month,
+            }
+        })
+    }, []);
+    
+    const handleMonthSelected = (month: string) => {
+        try {
+            const parseMonth = Number(month);
+            setMonthSelected(parseMonth);
+        } catch (error) {
+            throw new Error('Invalid month value. Is accept 0 - 24.');
+        }
+    }
+
+    const handleYearSelected = (year: string) => {
+        try {
+            const parseYear = Number(year);
+            setYearSelected(parseYear);
+        } catch (error) {
+            throw new Error('Invalid year value. Is accept integer numbers.');
+        }
+    }
 
     return (
         <Container>
             <ContentHeader title="Dashboard" lineColor="#F7931B">
-                <SelectInput options={options} onChange={() => {}}/>
+                <SelectInput
+                    options={months}
+                    defaultValue={monthSelected}
+                    onChange={(e) => handleMonthSelected(e.target.value)}
+                />
+                <SelectInput
+                    options={years}
+                    defaultValue={yearSelected}
+                    onChange={(e) => handleYearSelected(e.target.value)}
+                />
             </ContentHeader>
         </Container>
     );
