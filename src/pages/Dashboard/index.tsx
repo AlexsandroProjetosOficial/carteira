f0ec1ff3978f441c71df6e42b199ddd08f801c99
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-12-22 17:13:55 -0300

    Criando o cartão de status de carteira.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,9 +2,12 @@ import React, { useMemo, useState } from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import WalletBox from '../../components/WalletBox';
+import MessageBox from '../../components/MessageBox';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
 import listOfMonths from '../../utils/months';
+import happyImg from '../../assets/happy.svg';
+import sadImg from '../../assets/sad.svg';
 import { 
     Container,
     Content,
@@ -98,6 +101,13 @@ const Dashboard: React.FC = () => {
                     footerlabel = "atualizado com base nas entradas e saídas"
                     icon = "arrowDown"
                     color = "#E44c4E"
+                />
+
+                <MessageBox 
+                    title = "Muito bem!"
+                    icon = {happyImg}
+                    description = "Sua carteira está positiva!"
+                    footerText = "Continue assim. Considere investir o seu saldo."
                 />
             </Content>
         </Container>
