a9d5c3998262a446890164a91736653650394144
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-12-23 17:21:06 -0300

    Aplicando media queries nos componentes de layout e nos cartões de movimentos da carteira.

--- src/components/WalletBox/index.tsx
+++ src/components/WalletBox/index.tsx
@@ -39,9 +39,9 @@ const WalletBox: React.FC<IWalletBoxProps> = ({
         <Container color={color}>
             <span>{title}</span>
             <h1>
+                <strong>R$ </strong>
                 <CountUp 
                     end = {amount} // Número para parar bibliote encerrar.
-                    prefix = {"R$ "} // Coloca o sifrão da moeda BR.
                     separator = "." // Separador de milhar.
                     decimal = "," // Separador de casas decimais.
                     decimals = {2} // Informa quantas casas decimais vai ter.
